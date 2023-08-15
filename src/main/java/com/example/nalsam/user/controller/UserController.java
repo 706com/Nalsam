d35ccdef61192081f025a1585ef1b004c539a857
Kwak <ehdgusjdbswl@gmail.com>
2023-08-16 00:26:47 +0900

    feat - User Postmapping

--- src/main/java/com/example/nalsam/user/controller/UserController.java
+++ src/main/java/com/example/nalsam/user/controller/UserController.java
@@ -4,10 +4,7 @@ import com.example.nalsam.user.dto.request.TestRequest;
 import com.example.nalsam.user.dto.request.UserRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.example.nalsam.user.domain.Test;
 import com.example.nalsam.user.service.UserService;
 
