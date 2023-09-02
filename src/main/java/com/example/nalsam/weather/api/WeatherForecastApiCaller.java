49d418971496e51197e9fc5f522a3ba010524581
wjdgns4385 <wjdgns4385@naver.com>
2023-09-02 22:55:58 +0900

    Feat : 현재날씨 응답 데이터 추가 및 날씨예보api 개발
    1. 날씨 응답형식 변경(풍향,풍속 추가)
    2. 날씨 예보 api개발
    - 필요한 데이터 추출(하늘상태, 강수형태, 일최고기온, 일최저기온)
    - 3일치 데이터 제공(오늘,내일,모레)
    - 하늘상태, 강수형태는 오전, 오후만제공

--- src/main/java/com/example/nalsam/weather/api/WeatherForecastApiCaller.java
+++ src/main/java/com/example/nalsam/weather/api/WeatherForecastApiCaller.java
@@ -1,6 +1,7 @@
 package com.example.nalsam.weather.api;
 
 import com.example.nalsam.weather.dto.WeatherDto;
+import com.example.nalsam.weather.dto.WeatherForecastDto;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
@@ -15,27 +16,27 @@ import java.util.stream.Collectors;
 
 @Slf4j
 @Component
-public class CurrentWeatherApiCaller {
-    private final CurrentWeatherApi CurrentWeatherApi;
+public class WeatherForecastApiCaller {
+    private final WeatherForecastApi weatherForecastApi;
 
     @Value("${CURRENT-WEATHER-KEY}")
     private String SERVICE_KEY;
 
-    public CurrentWeatherApiCaller() {
+    public WeatherForecastApiCaller() {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/")
+                .baseUrl("https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/")
                 .addConverterFactory(JacksonConverterFactory.create(objectMapper))
                 .build();
 
-        this.CurrentWeatherApi = retrofit.create(CurrentWeatherApi.class);
+        this.weatherForecastApi = retrofit.create(WeatherForecastApi.class);
     }
 
-    public WeatherDto getCurrentWeather(String date , String time , String nx , String ny,String sido, String gu) {
+    public WeatherForecastDto getWeatherForecast(String date , String time , String nx , String ny, String sido, String gu) {
         try {
-            var call = CurrentWeatherApi.getCurrentWeather(SERVICE_KEY,"json",1,100,date, time, nx, ny);               // Todo : nx,ny 작업
+            var call = weatherForecastApi.getWeatherForecast(SERVICE_KEY,"json",1,1000,date, time, nx, ny);               // Todo : nx,ny 작업
             var response = call.execute().body();
 
             if (response == null || response.getResponse() == null) {
@@ -47,35 +48,37 @@ public class CurrentWeatherApiCaller {
                 return convert(response,sido,gu);
             }
 
-            throw new RuntimeException(" Weather 응답이 올바르지 않습니다. header=" + response.getResponse().getHeader());
+            throw new RuntimeException(" 날씨예보 응답이 올바르지 않습니다. header=" + response.getResponse().getHeader());
 
         } catch (IOException e) {
             log.error(e.getMessage(), e);
-            throw new RuntimeException(" Weather API error 발생! errorMessage=" + e.getMessage());
+            throw new RuntimeException(" 날씨예보 API error 발생! errorMessage=" + e.getMessage());
         }
     }
 
-    private WeatherDto convert(CurrentWeatherData.GetCurrentWeatherResponse response,String sido,String gu){
+    private WeatherForecastDto convert(WeatherForecastData.GetWeatherForecastResponse response,String sido,String gu){
 
         var item = response.getResponse().getBody().getItems().getItem();
 
         var weaherList = convert(item);
-        return WeatherDto.builder()
+        return WeatherForecastDto.builder()
                 .sido(sido)
                 .gu(gu)
-                .weatherInfoList(weaherList)
+                .weatherForecastInfos(weaherList)
                 .build();
 
     }
 
-    private List<WeatherDto.WeatherInfo> convert(List<CurrentWeatherData.Item> items) {
+    private List<WeatherForecastDto.WeatherForecastInfo> convert(List<WeatherForecastData.Item> items) {
         return items.stream()
-                .filter(item-> item.getCategory().equals("T1H")  || item.getCategory().equals("REH")
-                        || item.getCategory().equals("PTY") || item.getCategory().equals("RN1")
-                        || item.getCategory().equals("WSD")|| item.getCategory().equals("VEC"))
-                .map(item -> new WeatherDto.WeatherInfo(
+                .filter(item-> (item.getCategory().equals("TMX")  || item.getCategory().equals("TMN")
+                        || item.getCategory().equals("SKY") || item.getCategory().equals("PTY")) && (item.getFcstTime().equals("0600")
+                        || item.getFcstTime().equals("1500")))
+                .map(item -> new WeatherForecastDto.WeatherForecastInfo(
+                        item.getFcstDate(),
+                        item.getFcstTime(),
                         item.getCategory(),
-                        item.getObsrValue())
+                        item.getFcstValue())
                 )
                 .collect(Collectors.toList());
     }
--- src/main/java/com/example/nalsam/weather/dto/WeatherForecastDto.java
+++ src/main/java/com/example/nalsam/weather/dto/WeatherForecastDto.java
@@ -6,27 +6,34 @@ import lombok.Builder;
 import lombok.Getter;
 
 import java.util.List;
-
 @Getter
 @Builder
-public class WeatherDto {
+public class WeatherForecastDto {
     private String sido;
     private String gu;
-    private List<WeatherInfo> weatherInfoList;
+    private List<WeatherForecastInfo> weatherForecastInfos;
 
     @Getter
-    public static class WeatherInfo{
+    public static class WeatherForecastInfo{
+        private String date;
+        private String time;
         private WeatherCategory category;
         private String value;
 
-        public WeatherInfo(String category,String value){
+        public WeatherForecastInfo(String date,String time,String category,String value){
+            this.date=date;
+            this.time=time;
             this.category = WeatherCategoryUtil.getCategory(category);
-            if(category.equals("PTY")){
+            if(category.equals("SKY")){
+                this.value= WeatherCategoryUtil.getSkyValue(value);
+            }
+            else if(category.equals("PTY")){
                 this.value = WeatherCategoryUtil.getRainValue(value);
             }
-            else {
+            else{
                 this.value = value;
             }
         }
+
     }
 }
