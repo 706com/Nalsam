382afae8816c9d2de4788e8bfde27fe10f503f30
wjdgns4385 <wjdgns4385@naver.com>
2023-09-15 18:59:47 +0900

    Feat : chatGPT API를 활용한 챗봇 시스템 개발
    -chatGpt 3.5 turbo api 사용
    -키워드에 따른 고정적인 질문 요청

--- src/main/java/com/example/nalsam/chatbot/dto/request/HealthRequest.java
+++ src/main/java/com/example/nalsam/chatbot/dto/request/HealthRequest.java
@@ -1,9 +1,20 @@
 package com.example.nalsam.chatbot.dto.request;
 
+
 import lombok.Getter;
 
-@Getter
 public class HealthRequest {
+    public String getKeyword() {
+        return keyword;
+    }
+
+    public int getHeartRate() {
+        return heartRate;
+    }
+
+    public int getOxyzenSaturation() {
+        return oxyzenSaturation;
+    }
 
     private String keyword;
     private int heartRate;
--- src/main/java/com/example/nalsam/chatbot/service/ChatService.java
+++ src/main/java/com/example/nalsam/chatbot/service/ChatService.java
@@ -1,8 +1,10 @@
 package com.example.nalsam.chatbot.service;
 
+
 import com.example.nalsam.chatbot.dto.request.AirQualityRequest;
 import com.example.nalsam.chatbot.dto.request.HealthRequest;
-import io.github.flashvayne.chatgpt.service.ChatgptService;
+import com.example.nalsam.chatbot.dto.request.PlaceRecommend;
+import com.example.nalsam.chatbot.dto.request.SymtomCaution;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,27 +12,44 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ChatService {
 
-    private final ChatgptService chatgptService;
-
+    private final ChatCompletionService chatCompletionService;
     public String getChatResponse(HealthRequest healthRequest) {
 
         // 받은 정보로 질문 생성
-        String question = "현재 나의 체내 산소포화도가 " +
+        String question = "체내 산소포화도가 " +
                 healthRequest.getOxyzenSaturation() + "%이며 " +
-                "심박수는 " + healthRequest.getHeartRate() + "인데, 어떤 상태인지 자세히 알려줘";
+                "심박수는 " + healthRequest.getHeartRate() + "인데, 어떤 상태인지 5줄이내로 알려줘";
 
+        System.out.println(question);
+        String response =  chatCompletionService.chatCompletions(question);
         // Chat-GPT에게 질문 전송하여 응답 받기
-        String response = chatgptService.sendMessage(question);
         return response;
     }
     public String getChatResponse(AirQualityRequest airQualityRequest) {
 
         // 받은 정보로 질문 생성
-        String question ="현재 대기질이 so2:";
+        String question = "현재 대기질이 so2: " + airQualityRequest.getSo2Value()+", Co: "+airQualityRequest.getCoValue()
+                + ", No2: "+airQualityRequest.getNo2Value()+ ", O3: " +airQualityRequest.getO3Value() + ", pm10: "+ airQualityRequest.getPm10Value()
+                + ", pm25:" +airQualityRequest.getPm25Value()+" 인 상태인데 통합대기환경지수를 고려하여 각각 어떤 대기상태인지 간단하게 알려줘";
 
+        System.out.println(question);
         // Chat-GPT에게 질문 전송하여 응답 받기
-        String response = chatgptService.sendMessage(question);
-        return response;
+        return chatCompletionService.chatCompletions(question);
+    }
+    public String getChatResponse(PlaceRecommend placeRecommend) {
+
+        // 받은 정보로 질문 생성
+        String question = "대한민국 "+placeRecommend.getSido()+" "+ placeRecommend.getGu()+" 주변의 산책할만한 장소(공원, 산책로) 3가지를 간단하게 알려줘";
+        System.out.println(question);
+
+        return chatCompletionService.chatCompletions(question);
+    }
+    public String getChatResponse(SymtomCaution symtomCaution) {
+
+        // 받은 정보로 질문 생성
+        String question = "나는 " +symtomCaution.getSymtom()+"을 가지고있는데 주의해야할점을 5줄이내로 알려줘";
+        System.out.println(question);
+        return chatCompletionService.chatCompletions(question);
     }
 
 }
