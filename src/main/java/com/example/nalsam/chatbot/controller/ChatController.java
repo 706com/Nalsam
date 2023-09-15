9c476641a93e66ef9cb0d3e5b9c734e6c6d8884f
wjdgns4385 <wjdgns4385@naver.com>
2023-09-15 18:59:47 +0900

    Feat : chatGPT API를 활용한 챗봇 시스템 개발
    -chatGpt 3.5 turbo api 사용
    -키워드에 따른 고정적인 질문 요청

--- src/main/java/com/example/nalsam/chatbot/controller/ChatController.java
+++ src/main/java/com/example/nalsam/chatbot/controller/ChatController.java
@@ -1,12 +1,14 @@
 package com.example.nalsam.chatbot.controller;
 
-import com.example.nalsam.chatbot.dto.request.AirQualityRequest;
-import com.example.nalsam.chatbot.dto.request.HealthRequest;
+
+import com.example.nalsam.chatbot.dto.request.*;
+import com.example.nalsam.chatbot.service.ChatCompletionService;
 import com.example.nalsam.chatbot.service.ChatService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,15 +20,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/chatbot")
 public class ChatController {
     private final ChatService chatService;
-//    private final ChatgptService chatgptService;
 
+    private final ChatCompletionService chatCompletionService;
+
+    @PostMapping("")
+    public String chat(@RequestBody TestRequest testRequest){
+
+        if(!StringUtils.hasLength(testRequest.getQuestion())) {
+            throw new RuntimeException();
+        }
+
+        return chatCompletionService.chatCompletions(testRequest.getQuestion());
+    }
     //chat-gpt 와 간단한 채팅 서비스 소스
     @PostMapping("/healthStatus")
-    public ResponseEntity<String> chat(@RequestBody HealthRequest healthRequest){
+    public ResponseEntity<String> chat(@RequestBody HealthRequest healthRequest) {
         try {
-
             String response = chatService.getChatResponse(healthRequest);
 
+            System.out.println("응답 : "+response);
             // 응답을 프론트엔드로 POST로 반환
             return ResponseEntity.ok(response);
         } catch (Exception e) {
@@ -39,6 +51,7 @@ public class ChatController {
         try {
 
             String response = chatService.getChatResponse(airQualityRequest);
+            System.out.println("응답 : "+response);
 
             // 응답을 프론트엔드로 POST로 반환
             return ResponseEntity.ok(response);
@@ -47,5 +60,32 @@ public class ChatController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error processing the question.");
         }
     }
+    @PostMapping("/placeRecommend")
+    public ResponseEntity<String> chat(@RequestBody PlaceRecommend placeRecommend){
+        try {
 
+            String response = chatService.getChatResponse(placeRecommend);
+            System.out.println("응답 : "+response);
+
+            // 응답을 프론트엔드로 POST로 반환
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            // 오류 발생 시 오류 응답 반환
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error processing the question.");
+        }
+    }
+    @PostMapping("/symytomCaution")
+    public ResponseEntity<String> chat(@RequestBody SymtomCaution symtomCaution){
+        try {
+
+            String response = chatService.getChatResponse(symtomCaution);
+            System.out.println("응답 : "+response);
+
+            // 응답을 프론트엔드로 POST로 반환
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            // 오류 발생 시 오류 응답 반환
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error processing the question.");
+        }
+    }
 }
---
+++ src/main/java/com/example/nalsam/chatbot/dto/request/SymtomCaution.java
@@ -0,0 +1,9 @@
+package com.example.nalsam.chatbot.dto.request;
+
+import lombok.Getter;
+
+@Getter
+public class SymtomCaution {
+    private String keyword;
+    private String symtom;
+}
