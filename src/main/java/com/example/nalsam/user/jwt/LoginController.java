f3ceab47be3bc7bf3b267c59ff2db896b6213f9f
wjdgns4385 <wjdgns4385@naver.com>
2023-09-16 17:30:59 +0900

    [fix] : 코드 복구

--- src/main/java/com/example/nalsam/user/jwt/LoginController.java
+++ src/main/java/com/example/nalsam/user/jwt/LoginController.java
@@ -2,9 +2,13 @@ package com.example.nalsam.user.jwt;
 
 import com.example.nalsam.user.dto.request.LoginRequest;
 import com.example.nalsam.user.dto.request.UserRequest;
+import com.example.nalsam.user.dto.response.UserResponse;
 import com.example.nalsam.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,18 +16,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 @Controller
+@RequiredArgsConstructor
 public class LoginController {
 
     private final LoginService loginService;
 
-    public LoginController(LoginService loginService) {
-        this.loginService = loginService;
-    }
-
+    private final JwtTokenProvider jwtTokenProvider;
 
     @PostMapping("/login") // http 서블릿 세션 로그인
     public ResponseEntity<JwtToken> login(@RequestBody LoginRequest loginRequest) {
 
+//        System.out.println("1");
         JwtToken token = loginService.login(loginRequest);
 
         return ResponseEntity.ok().body(token);
@@ -35,22 +38,31 @@ public class LoginController {
     public ResponseEntity<Void> signup(@RequestBody UserRequest userRequest){
 
         loginService.createUser(userRequest);
-
         return ResponseEntity.noContent().build();
     }
 
-//    @PostMapping("/logout") // http 서블릿 세션 로그아웃
-//    public ResponseEntity<Void> logout(HttpServletRequest request){
+    @PostMapping("/home") // http 서블릿 세션 로그아웃
+    public ResponseEntity<UserResponse> home(HttpServletRequest request){
+
+        String accessToken = jwtTokenProvider.getAccessToken(request);
+
+        return ResponseEntity.ok().body(loginService.getUserResponse(accessToken));
+    }
+
+
+    @PostMapping("/mainTest") // http 서블릿 세션 로그아웃
+    public ResponseEntity<UserResponse> main(){
+
+        return ResponseEntity.ok().body(loginService.mainTest());
+    }
+
+
+
+//    @PostMapping("/main2") // http 서블릿 세션 로그아웃
+//    public ResponseEntity<UserResponse> main2(HttpServletRequest request){
 //
-//        HttpSession session = request.getSession(false);
+//        request.getHeader("Authorization");
 //
-////        // 세션이 없으면 리다이렉트
-////        if(session == null){
-////            return "redirect:/";
-////        }
-//
-//        // invalidate 는 세션을 삭제하는 기능
-//        session.invalidate();
-//        return ResponseEntity.noContent().build();
+//        return ResponseEntity.ok().body(loginService.main2());
 //    }
 }
