11ee85b4e0cff0b2feab516982ae877f82f67738
Kwak <ehdgusjdbswl@gmail.com>
2023-10-25 17:12:39 +0900

    [feat] : 융복합 컨트롤러 생성

--- src/main/java/com/example/nalsam/convergence/controller/ConvergenceController.java
+++ src/main/java/com/example/nalsam/convergence/controller/ConvergenceController.java
@@ -2,7 +2,7 @@ package com.example.nalsam.convergence.controller;
 
 import com.example.nalsam.convergence.dto.ConvergenceRequest;
 import com.example.nalsam.convergence.dto.ConvergenceResponse;
-import com.example.nalsam.convergence.service.OutingService;
+import com.example.nalsam.convergence.service.ConvergenceService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,15 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/convergence")
 public class ConvergenceController {
     // Todo : 객체 요청 -> 대기질 6가지, 날씨 3가지 , 사용자정보 6가지
-    private OutingService outingService;
+    private ConvergenceService convergenceService;
 
-    public ConvergenceController(OutingService outingService) {
-        this.outingService = outingService;
+    public ConvergenceController(ConvergenceService convergenceService) {
+        this.convergenceService = convergenceService;
     }
     
     @GetMapping("/get")
     ResponseEntity<ConvergenceResponse> getConvergenceData(@RequestBody ConvergenceRequest request){
-        outingService.method(request);
+        convergenceService.method(request);
         //Todo : Response 안에 넣어야 할 데이터 : outingService 에서 점수, String 출력
         return ResponseEntity.ok( "RESPONSE 넣기");
     }
