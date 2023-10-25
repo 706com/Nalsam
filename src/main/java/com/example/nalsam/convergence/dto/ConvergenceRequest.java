e727e40d91b7987f5884c5d9e464efb4dae59c6c
wjdgns4385 <wjdgns4385@naver.com>
2023-10-25 17:10:20 +0900

    [feat] : 융복합 요청 및 응답 기능구현

--- src/main/java/com/example/nalsam/convergence/dto/ConvergenceRequest.java
+++ src/main/java/com/example/nalsam/convergence/dto/ConvergenceRequest.java
@@ -1,7 +1,29 @@
 package com.example.nalsam.convergence.dto;
 
+import com.example.nalsam.airquality.util.AirQualityGrade;
+import lombok.Getter;
+
+@Getter
 public class ConvergenceRequest {
 
-    //Todo : 6 + 3+ 6 개 의 데이터 담기
+    //대기질 데이터
+    private String pm10Grade ;  //미세먼지
+    private String pm25Grade;   //
+    private String so2Grade ;
+    private String o3Grade ;
+    private String no2Grade;
+    private String coGrade;
+
+    //날씨 데이터
+    private Double temperature; //기온
+    private int precipitation; //강수량
+    private int Humidity;       //습도
+
+    //사용자 데이터
+    private String age; //나이
+    private String heartRate; //심박수
+    private String oxygenSaturation; // 산소포화도
+    private String Symtom; //증상
+
 
 }
