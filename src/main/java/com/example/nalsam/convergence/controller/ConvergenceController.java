b42feaa8ea4cb37c824a20c663c4cf610c1e3ef5
Kwak <ehdgusjdbswl@gmail.com>
2023-10-25 17:10:38 +0900

    [feat] : 융복합 컨트롤러

--- src/main/java/com/example/nalsam/convergence/controller/ConvergenceController.java
+++ src/main/java/com/example/nalsam/convergence/controller/ConvergenceController.java
@@ -1,11 +1,29 @@
 package com.example.nalsam.convergence.controller;
 
+import com.example.nalsam.convergence.dto.ConvergenceRequest;
+import com.example.nalsam.convergence.dto.ConvergenceResponse;
+import com.example.nalsam.convergence.service.OutingService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/convergence")
 public class ConvergenceController {
     // Todo : 객체 요청 -> 대기질 6가지, 날씨 3가지 , 사용자정보 6가지
+    private OutingService outingService;
 
-    // 질환
-    // 질환별로 service
+    public ConvergenceController(OutingService outingService) {
+        this.outingService = outingService;
+    }
+    
+    @GetMapping("/get")
+    ResponseEntity<ConvergenceResponse> getConvergenceData(@RequestBody ConvergenceRequest request){
+        outingService.method(request);
+        //Todo : Response 안에 넣어야 할 데이터 : outingService 에서 점수, String 출력
+        return ResponseEntity.ok( "RESPONSE 넣기");
+    }
+    
 }
