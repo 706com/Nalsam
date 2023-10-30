47eb599882d7a5aea03ef8e77631f0c4ae01a083
Bae <baeee0420@gmail.com>
2023-10-30 22:06:40 +0900

    feet:상태 점수

---
+++ src/main/java/com/example/nalsam/convergence/dto/StatusScore.java
@@ -0,0 +1,42 @@
+package com.example.nalsam.convergence.dto;
+
+public class StatusScore {
+	
+	private int age;
+	private String symtom[];
+	private int score = 0;
+	
+	StatusScore(int age, String symtomStr){
+		this.age = age;
+		this.symtom = symtomStr.trim().split(",");
+	}
+	
+	public int measureStatus() {
+		boolean flag = false;
+		
+		for(String i : symtom) {
+			if(i.equals("천식")) {
+				flag = true;
+			}
+		}
+		
+		if(flag) {
+			if(age < 16) {
+				score += 6;
+			}else if(age > 15 && age < 61) {
+				score += 10;
+			}else {
+				score += 4;
+			}
+		}else {
+			if(age < 16) {
+				score += 8;
+			}else if(age > 15 && age < 61) {
+				score += 10;
+			} else {
+				score += 7;
+			}
+		}
+		return score;
+	}
+}
