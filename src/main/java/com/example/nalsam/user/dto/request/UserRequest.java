8b083564dc119cd0f1b628d7810193496817cc55
Kwak <ehdgusjdbswl@gmail.com>
2024-02-10 18:46:55 +0900

    [refactor] : 빌더패턴 삭제

--- src/main/java/com/example/nalsam/user/dto/request/UserRequest.java
+++ src/main/java/com/example/nalsam/user/dto/request/UserRequest.java
@@ -1,17 +1,13 @@
 package com.example.nalsam.user.dto.request;
 
 
-import lombok.AccessLevel;
-import lombok.Builder;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserRequest {
 
     private String loginId;
@@ -26,14 +22,4 @@ public class UserRequest {
 
     private String symptom;
 
-
-    @Builder
-    public UserRequest(String loginId, String password, String userName,String birthDate, Integer isMale, String symptom) {
-        this.loginId = loginId;
-        this.password = password;
-        this.userName = userName;
-        this.birthDate = birthDate;
-        this.isMale = isMale;
-        this.symptom = symptom;
-    }
 }
