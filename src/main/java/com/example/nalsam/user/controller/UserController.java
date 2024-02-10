063d8c28322018358f0a6ebc72126cc1f01f2803
Kwak <ehdgusjdbswl@gmail.com>
2024-02-10 17:06:15 +0900

    [refactor] : REST API로 변경

--- src/main/java/com/example/nalsam/user/controller/UserController.java
+++ src/main/java/com/example/nalsam/user/controller/UserController.java
@@ -1,7 +1,6 @@
 package com.example.nalsam.user.controller;
 
 import com.example.nalsam.user.domain.Users;
-import com.example.nalsam.user.dto.request.TestRequest;
 import com.example.nalsam.user.dto.request.UserDeletionRequest;
 import com.example.nalsam.user.dto.request.UserPasswordRequest;
 import com.example.nalsam.user.dto.request.UserRequest;
@@ -20,52 +19,39 @@ public class UserController {
 
     private final UserService userService;
 
-    // < 회원 저장 TEST >
-    @PostMapping("/test")
-    public ResponseEntity<Users> createUserTest(@RequestBody TestRequest testRequest){
-
-        return ResponseEntity.ok().body(userService.getUserTest(testRequest));
-    }
-
     // < 회원 저장 API >
-    @PostMapping("/save")
+    @PostMapping("")
     public ResponseEntity<Void> createUser(@RequestBody UserRequest userRequest){
-
         userService.saveUserProfile(userRequest);
-
         return ResponseEntity.noContent().build();
     }
 
     // < 회원 조회 API >
     // 회원 로그인ID 로 조회
-    @GetMapping("/show")
-    public ResponseEntity<UserResponse> showUserInfo(@RequestParam("id") String loginId){
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponse> showUserInfo(@PathVariable("id") String loginId){
         return ResponseEntity.ok(userService.showUserInfoById(loginId));
     }
 
     // 회원 전체 조회
-    @GetMapping("/show/all")
+    @GetMapping("")
     public ResponseEntity<List<UserResponse>> showUserInfoAll(){
         return ResponseEntity.ok(userService.showUserInfoAll());
     }
 
     // < 회원 수정 API >
     // password 수정
-    @PutMapping("/update/password")
+    @PutMapping("/password")
     public ResponseEntity<Void> updateUserPassword(@RequestBody UserPasswordRequest request){
-
         userService.updateUserPassword(request);
-
         return ResponseEntity.noContent().build();
     }
 
     // < 회원 삭제 API >
     // 아이디 패스워드 일치시.
-    @DeleteMapping("/delete")
+    @DeleteMapping("")
     public ResponseEntity<Void> deleteUser(@RequestBody UserDeletionRequest request){
-
         userService.deleteUser(request);
-
         return ResponseEntity.noContent().build();
     }
 }
