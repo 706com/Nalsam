3623e1fc79b95c486a86e99cbf0405d87a096e08
Kwak <ehdgusjdbswl@gmail.com>
2024-02-14 19:25:41 +0900

    [style] : doFilter 이해 및 주석 정리

--- src/main/java/com/example/nalsam/user/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/example/nalsam/user/jwt/JwtAuthenticationFilter.java
@@ -1,6 +1,7 @@
 package com.example.nalsam.user.jwt;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.GenericFilterBean;
@@ -13,50 +14,42 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
-/*
+/* <JWT 인증과정>
 Jwt 인증을 위해 생성되는 토큰. 요청과 함께 바로 실행됨.
 요청이 들어오면 헤더에서 토큰 추출.
 그리고 유효성 검사.
  */
 
 @RequiredArgsConstructor
+@Slf4j
 public class JwtAuthenticationFilter extends GenericFilterBean {
 
     private final JwtTokenProvider jwtTokenProvider;
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-//        System.out.println("a");
-        // Header 에서 jwt 받아오기
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+        // 1. Request Header 에서 jwt 받아오기
         String token = resolveToken((HttpServletRequest) request);
-//        System.out.println("b");
-        // 유효한 토큰인지 검증 (accessToken을 가져와 복호화하고 검증)
-        // 검증이 모두 끝나면 토큰을 인증받은 유저인 UsernamePasswordAuthenticationToken을 리턴
-        // 과정이 끝나면, 이 유저는 토큰이 유효한 유저임이 증명되고 SecurityContext 에 저장
+        // 토큰을 가지고 있는지와 && 토큰이 유효한 것인지 검증
         if(token != null && jwtTokenProvider.validateToken(token)){
-//            System.out.println("c");
-            // 유효한 토큰이면 토큰으로부터 유저정보 받아오기.
+            // 검증이 모두 끝나면(유효한 토큰이면)
+            // 토큰을 인증받은 유저인 UsernamePasswordAuthenticationToken을 리턴
             Authentication authentication = jwtTokenProvider.getAuthentication(token);
-//            System.out.println("d");
-            // SecurityContext 에 Authentication 객체 저장.
+            // SecurityContext 안에 Authentication 객체 저장.
+            // == 과정이 끝나면, 이 유저는 토큰이 유효한 유저임이 증명되고 SecurityContext 에 저장
             SecurityContextHolder.getContext().setAuthentication(authentication);
-//            System.out.println("e");
         }
-//        System.out.println("f");
-        chain.doFilter(request,response);
+        filterChain.doFilter(request,response);
     }
 
-    //Request의 Header에서 token 값 가져오기.
+    // Request의 Header에서 jwt 가져오기.
     public String resolveToken(HttpServletRequest request){
-
-//        System.out.println("A");
         //Authorization 이 Bearer 임을 확인
         String bearerToken = request.getHeader("Authorization");
         if(StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer")){
-//            System.out.println("B");
             return bearerToken.substring(7);
         }
-//        System.out.println("C");
+        log.debug("토큰이 존재하지 않습니다.");
         return null;
     }
 }
